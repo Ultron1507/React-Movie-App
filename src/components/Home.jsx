be9b874761cabd0c11b5a4ecdf32f56743c04f1a
Puspita Das <puspita@example.com>
2025-02-26 18:22:41 +0530

    Initial commit for React-Movie-App

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidenav from './templates/Sidenav';
+
+const Home = () => {
+    document.title = "Homepage";
+  return (
+    <>
+    <Sidenav/>
+    <div className='w-[80%] h-full'></div>
+    
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/templates/Sidenav.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Sidenav = () => {
+  return (
+    <div className='w-[20%] h-full border-r-2 border-zinc-400 p-10'>
+        <h1 className='text-2xl text-white font-bold'>
+        <i class=" text-[#6556CD] ri-tv-fill mr-2"></i>
+        <span>StreamX</span> 
+        </h1>
+
+        <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
+        <h1 className='text-white font-semibold text-xl mt-10 mb-5'>
+            New Feeds
+            </h1>
+        <lin className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
+        <i class=" ri-fire-fill"></i> Trending
+            </lin>
+        <lin className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
+        <i class="mr-2 ri-sparkling-fill"></i> Popular
+            </lin>
+        <lin className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
+        <i class="mr-2 ri-movie-2-fill"></i>Movies
+        </lin>
+        <lin className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
+        <i class="mr-2 ri-tv-2-fill"></i>Tv shows
+        </lin>
+        <lin className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
+        <i class="mr-2 ri-team-fill"></i>People
+        </lin>
+        </nav>
+        <hr className='border-none h-[1px] bg-zinc-400'/>
+        <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
+        <h1 className='text-white font-semibold text-xl mt-10 mb-5'>
+            Website Information
+            </h1>
+        <lin className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
+        <i class="mr-2 ri-information-2-fill"></i> About StreamX
+            </lin>
+        <lin className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
+        <i class="mr-2 ri-phone-fill"></i> Contact Us
+            </lin>
+        </nav>
+    </div>
+  )
+}
+
+export default Sidenav
